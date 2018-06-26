f46ee4b9007b7762414759afd6a9bac2927b6497
Admin <aloon8@gmail.com>
2018-06-26 16:42:07 +0300

    22:30

---
+++ MyExceptions.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 26/06/2018.
+//
+
+#ifndef EXERCISE3_MYEXCEPTIONS_H
+#define EXERCISE3_MYEXCEPTIONS_H
+
+
+class MyExceptions {
+
+};
+
+
+#endif //EXERCISE3_MYEXCEPTIONS_H
