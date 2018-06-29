da3540d5bb240ce6c2cc97801522964067bb3743
Admin <aloon8@gmail.com>
2018-06-29 16:13:00 +0300

    asd

--- Cruiser.cpp
+++ Cruiser.cpp
@@ -15,21 +15,21 @@ void Cruiser::update() {
 }
 
 void Cruiser::attack(std::weak_ptr<Ship> ship){
-    if(range > trackBase.getPosition().distance(ship.lock()->getTrackBase().getPosition())){
+    if(range < trackBase.getPosition().distance(ship.lock()->getTrackBase().getPosition())){
         throw MyExceptions::OutOfRangeException("The ship is out of range for attack");
     }
     ship.lock()->setStatus(Stopped);
-    if(myType == Ship::TypeIdShip::FR){
+    if(ship.lock()->getTypeName() == Ship::TypeIdShip::FR) {
         auto fr = dynamic_pointer_cast<Freighter>(ship.lock());
 
-        if(Force_of_attack > fr->getResistance()){
+        if (Force_of_attack > fr->getResistance()) {
             Force_of_attack++;
             fr->setContainers(0);
-        }
-        else
+        } else {
             Force_of_attack--;
+        }
     }
-    else if(myType == Ship::TypeIdShip::PB){
+    else if(ship.lock()->getTypeName() == Ship::TypeIdShip::PB){
         auto fr = dynamic_pointer_cast<PatrolBoat>(ship.lock());
         if(Force_of_attack > fr->getResistance()){
             Force_of_attack++;
@@ -39,6 +39,7 @@ void Cruiser::attack(std::weak_ptr<Ship> ship){
             Force_of_attack--;
             fr->setResistance(fr->getResistance()+1);
         }
+        fr->setNumOfMoves(3);
     }
 }
 
